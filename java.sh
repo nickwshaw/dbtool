3bb1c8171825e822b1be88aa4e6dc87d19949822
Nicholas Shaw <nicholas.shaw@dixonscarphone.com>
2018-05-17 14:44:17 +0100

    Updated update and build number

--- java.sh
+++ java.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
 # Dependencies: wget, ca-certificates, unzip
 # ENV to set in target Dockerfile: JAVA_HOME="/opt/java"
 
 
 JAVA_VERSION=8
-JAVA_UPDATE=144
-JAVA_BUILD=01
+JAVA_UPDATE=171
+JAVA_BUILD=11
 JAVA_HOME="/opt/java"
-wget -P "/tmp/java_packages/" "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jre-8u144-linux-x64.tar.gz"
+
+wget -nv -P "/tmp/java_packages/" --header "Cookie: oraclelicense=accept-securebackup-cookie;" \
+    "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}u${JAVA_UPDATE}-b${JAVA_BUILD}/090f390dda5b47b9b721c7dfaa008135/jre-${JAVA_VERSION}u${JAVA_UPDATE}-linux-x64.tar.gz"
 tar -xzf "/tmp/java_packages/jre-${JAVA_VERSION}u${JAVA_UPDATE}-linux-x64.tar.gz" -C "/tmp/java_packages/"
 mkdir -p /opt
 mv "/tmp/java_packages/jre1.${JAVA_VERSION}.0_${JAVA_UPDATE}" "${JAVA_HOME}"
